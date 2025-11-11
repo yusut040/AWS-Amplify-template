0fd8e457f01fc6ffccd75c300d57ef493063cd0a
Yusuke <yusuweb@gmail.com>
2025-11-11 22:31:26 +0900

    Initial commit

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,6 +3,6 @@ import { defineStorage } from "@aws-amplify/backend";
 export const storage = defineStorage({
   name: "storage-browser-test",
   access: (allow: any) => ({
-    '**/*': [allow.authenticated.to(['read', 'write', 'delete'])],
+    'public/*': [allow.authenticated.to(['read', 'write', 'delete'])],
   }),
 });
