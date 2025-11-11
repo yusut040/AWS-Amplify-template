e930d5769c413540eb0a67e9dd28bf48bb069759
Yusuke <yusuweb@gmail.com>
2025-11-11 22:50:00 +0900

    Update storage access pattern to support dynamic company folders

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,6 +3,8 @@ import { defineStorage } from "@aws-amplify/backend";
 export const storage = defineStorage({
   name: "storage-browser-test",
   access: (allow: any) => ({
-    'public/*': [allow.authenticated.to(['read', 'write', 'delete'])],
+    'public/{company_name}/*': [
+      allow.authenticated.to(['read', 'write', 'delete'])
+    ],
   }),
 });
